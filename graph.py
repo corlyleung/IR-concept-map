c741bf84f6bf2a5ba6d03c436585906b83c2ea33
Corly <corly@Corlys-MacBook-Pro.local>
2016-11-27 20:31:45 -0600

    updated background variable

--- graph.py
+++ graph.py
@@ -31,12 +31,10 @@ def cal_cost (concepts, source, background):
 
 def cal_cost_helper(concepts, source,  background, depth, path):
 	cur_path = []
-	cur_path.append(source)
-	depth += 1
-	#leaf nodes
+	cur_depth = 1
+	
 	if source not in concepts:
-		return depth, path + cur_path
-
+		return depth, path
 	cur_edge = concepts[source]
 
 	#all the depends on path excluding background nodes
@@ -44,23 +42,29 @@ def cal_cost_helper(concepts, source,  background, depth, path):
 		if cur_depend in background:
 			continue
 		cur_path.append(cur_depend)
+		child_depth, child_path = cal_cost_helper(concepts, cur_depend, background, depth + 1, [])
+		if depth < child_depth:
+			depth = child_depth
 
 	#return value of all depends on
 	if len(cur_edge.instance) == 0:
-		return depth+1, path + cur_path
+		return cur_depth+depth, path + cur_path
 
 	#current max cost of all depends on
-	max_cost = math.pow(len(cur_path), -1*depth)
+	max_cost = 0
+	if len(cur_path) != 0:
+		max_cost = math.pow(len(cur_path), -1*depth)
 
 	for child in cur_edge.instance:
 		if child in background:
-			return depth-1, path
-		child_depth, temp_path = cal_cost_helper(concepts, child, background, depth, [])
+			return depth, path
+		child_depth, temp_path = cal_cost_helper(concepts, child, background, 0, [])
 		child_cost = math.pow(len(temp_path), -1*child_depth)
 		if child_cost > max_cost:
 			max_cost = child_cost
 			cur_path = temp_path
-	return depth, path + cur_path
+			cur_depth = child_depth
+	return depth+cur_depth, path + cur_path
 
 concepts = make_graph("test_concept.csv")
 to_learn = "vector_space_model"
